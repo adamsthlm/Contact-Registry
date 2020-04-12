707cccd465e989439423cefd4a6cdfd78198467c
Carl-Adam Berglund <adam@carl-adam.tech>
2020-04-12 16:14:19 +0200

    class Address first commit, starting on ContactManager next time.

--- Address.cs
+++ Address.cs
@@ -15,15 +15,24 @@ namespace Contact_Registry
         private string _street = string.Empty;
         private string _zipCode = string.Empty;
         private string city = string.Empty;
+        private Address address; // skapad för att få theOther att funka
 
         // properties
         public string City { get => city; set => city = value; }
         public Countries Country { get; set; }
         public string Street { get; set; }
         public string ZipCode { get; set; }
+        // public Address Address { get; set; } // I'm unsure about this property 
+
 
         // default constructor
         public Address() { }
+
+        //// constructor for theOther i.e. 1 parameter 
+        //public Address(Address)
+        //{
+
+        //}
 
         // constructor 3 parameters
         public Address(string street, string zipCode, string city)
@@ -40,6 +49,13 @@ namespace Contact_Registry
             _zipCode = ZipCode;
             _city = City;
             _country = Country;
+        }
+
+
+        // skapad för att få theOther att funka
+        public Address(Address address)
+        {
+            this.address = address;
         }
     } // class 
 } // namespace
--- Contact.cs
+++ Contact.cs
@@ -9,6 +9,79 @@ namespace Contact_Registry
 {
     class Contact
     {
+        // Fields
+        private string _firstname = string.Empty;
+        private string _lastname = string.Empty;
+        // private Address _address = new Address();
+
+        // Aggregation
+        private Address address;
+
+        // Properties
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Fullname
+        {
+            get { return Firstname + " " + Lastname; }
+        }
+
+
+
+
+        // default constructor
+        public Contact() { address = new Address(); }
+
+        // constructor w. 3 params.
+        public Contact(string firstname, string lastname, Address adr)
+        {
+            _firstname = Firstname; // Field _firstname is set via property Firstname
+            _lastname = Lastname; // Field _lastname is set via property Lastname
+            this.address = adr; // one Address object
+        }
+
+        // theOther constructor for copying objects
+        public Contact(Contact theOther)
+        {
+            _firstname = theOther._firstname;
+            _lastname = theOther._lastname;
+            this.address = new Address(theOther.address); // för att få denna rad att fungera skapade jag fält och konstruktor i klassen Adress
+
+        }
+
+        public bool ValidateData()
+        {
+
+            bool ok = true;
+
+            if (ok)
+            {
+                if (_firstname.Equals(string.Empty))
+                {
+                    ok = false;
+                    return ok;
+                } else
+
+                if (_lastname.Equals(string.Empty))
+                {
+                    ok = false;
+                    return ok;
+                } else
+
+                if (!address.Equals(this.address))
+                {
+                    ok = false;
+                }
+                return ok;
+            }
+            return ok;
+        }
+
+        public override string ToString()
+        {
+            string strOut = string.Format("{0,-20} {1}", Fullname, address.ToString());
+            return strOut;
+
+        }
 
     } // class 
 }// namespace 
